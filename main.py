ff3415b24b58192764381f8d6f29ebccf46dfc89
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2024-10-22 15:29:08 +0600

    Added requirements.txt file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,7 +8,22 @@ app = FastAPI()
 async def read_root():
     return {'message:' 'Hello world'}
 
+@app.get('/greet')
+async def greet(name: Optional[str] = 'User', age: int = 0) -> object:
+    return {'message:' f'hello {name} your age is: {age}'}
+
 @app.get('/my-fav-book-list')
 async def fav_book_lists():
     fav_list = ['1. Deyal', '2. Atomic habits']
     return fav_list
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+@app.post('/book-create')
+async def book_create(book_data: BookCreateModel):
+    return {
+        'title': book_data.title,
+        'author': book_data.author
+    }
