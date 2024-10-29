d49de7df8d65dec55c219577341249b51440f65b
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2024-10-29 13:47:38 +0600

    added login functionality, update requirements.txt file & created class with env variable for JWTCOnfig

--- src/config.py
+++ src/config.py
@@ -2,15 +2,21 @@ from dotenv import load_dotenv
 from urllib.parse import quote_plus
 import os
 
-class DatabaseConfig:
-    load_dotenv()
+load_dotenv()
 
-    user = os.getenv("DB_USER")
-    # password = os.getenv("DB_PASSWORD")
-    password = quote_plus(os.getenv("DB_PASSWORD"))
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    db_name = os.getenv("DB_NAME")
+class DatabaseConfig:
+    user = os.getenv('DB_USER')
+    password = quote_plus(os.getenv('DB_PASSWORD'))
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    db_name = os.getenv('DB_NAME')
 
     DATABASE_URL = f'postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}'
+
+class JWTConfig:
+    jwt_secret = os.getenv('JWT_SECRET')
+    jwt_algorithm = os.getenv('JWT_ALGORITHM')
+    jwt_access_token_expiry = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRY'))
+    jwt_refresh_token_expiry = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRY'))
+
 
