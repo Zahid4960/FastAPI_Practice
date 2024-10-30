2adef45302dc145b1e0aad0ec76db76c348a1af8
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2024-10-30 15:58:10 +0600

    added role based authentication & completed auth feature

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -6,13 +6,14 @@ from datetime import timedelta, datetime
 from .schemas import UserModel, UserCreateModel, UserLoginModel
 from .service import UserService
 from .utils import create_access_token, decode_token, verify_password
-from .dependencies import RefreshTokenBearer, AccessTokenBearer, get_current_user
+from .dependencies import RefreshTokenBearer, AccessTokenBearer, get_current_user, RoleChecker
 from src.db.main import get_session
 from src.db.redis import add_jti_to_blocklist
 from src.config import JWTConfig
 
 auth_router = APIRouter()
 user_service = UserService()
+role_checker = RoleChecker(['admin', 'user'])
 
 @auth_router.post('/signup', response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user_account(user_data: UserCreateModel, session: AsyncSession = Depends(get_session)):
@@ -41,7 +42,8 @@ async def login_users(login_data: UserLoginModel, session: AsyncSession = Depend
             access_token = create_access_token(
                 user_data={
                     'email': user.email,
-                    'user_uid': str(user.uid)
+                    'user_uid': str(user.uid),
+                    'role': user.role
                 }
             )
 
@@ -82,7 +84,7 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid or expired token')
 
 @auth_router.get('/me')
-async def get_current_user(user=Depends(get_current_user)):
+async def get_current_user(user=Depends(get_current_user), _: bool = Depends(role_checker)):
     return user
 
 @auth_router.get('/logout')
