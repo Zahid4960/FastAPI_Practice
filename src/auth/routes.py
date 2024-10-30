b5e6bfeb44c8c603ca337a94368285ea780fb6af
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2024-10-30 10:57:39 +0600

    Worked with aceess token & refresh token

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-from datetime import timedelta
+from datetime import timedelta, datetime
 from .schemas import UserModel, UserCreateModel, UserLoginModel
 from .service import UserService
 from .utils import create_access_token, decode_token, verify_password
+from .dependencies import RefreshTokenBearer
 from src.db.main import get_session
 from src.config import JWTConfig
 
@@ -65,3 +66,16 @@ async def login_users(login_data: UserLoginModel, session: AsyncSession = Depend
             )
 
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid email or password')
+
+@auth_router.get('/refresh_token')
+async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details['exp']
+
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(user_data=token_details['user'])
+
+        return JSONResponse(content={
+            'access_token': new_access_token
+        })
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid or expired token')
